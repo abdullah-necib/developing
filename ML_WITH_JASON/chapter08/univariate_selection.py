8b492b90dbde885e148b0281220005f9b7bb5150
Abdullah <necib.abdullah@gmail.com>
2020-09-29 20:54:48 +0300

    after creating the file ~/Desktop/programming/CS50_webapplications_with_python/test/using_decorator.py

--- ML_WITH_JASON/chapter08/univariate_selection.py
+++ ML_WITH_JASON/chapter08/univariate_selection.py
@@ -6,3 +6,14 @@ filename = 'datasets_228_482_diabetes_with_header.csv'
 fpath = '/'.join(__file__.split('/')[:-2])+'/data/'+filename
 names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
 dataframe = read_csv(fpath, names=names, header=0)
+array = dataframe.values
+x = array[:,0:8]
+y = array[:,8]
+# feature extraction
+test = SelectKBest(score_func=chi2, k=4)
+fit = test.fit(x,y)
+# summurize score
+set_printoptions(precision=3)
+print(fit.scores_)
+features = fit.transform(x)
+print(features[0:5,:])
