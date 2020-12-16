50f4d02419d5fbe396d0112678c2f133ebeadd4c
Abdullah <necib.abdullah@gmail.com>
2020-12-16 17:24:40 +0300

    2020_12_16

--- Math_project/statistics_110/startigies_01.py
+++ Math_project/statistics_110/startigies_01.py
@@ -7,11 +7,25 @@ Created on Fri Nov  6 05:23:19 2020
 """
 from math import factorial
 from itertools import combinations
+from itertools import combinations_with_replacement
 from itertools import permutations
 from itertools import product
 
+def write_to_file(lst, filename='test.log',mode='w'):
+    l= len(str(len(lst)))
+    with open(filename, mode) as f:
+        count = 1
+        for i in lst:
+            f.write(f'{count:{l}} >> {i}\n')
+            count += 1
+    print(f'done writing to file: {filename}')
+    
+    
 def comb(n,r):
     return int(factorial(n)/(factorial(n-r)*factorial(r)))
+
+def perm(n,r):
+    return factorial(r)*comb(n,r)
 
 def birthday_problem(k):
     """this is from third licture in statistics 101 """
@@ -110,6 +124,7 @@ def zrief():
     return result
 
 def rBall_nBox(r,n):
+    """r is the quantity of balls"""
     base = [x for x in range(r+1)]
     main_space = [list(x) for x in product(base,repeat = n) if sum(list(x))== r]
     fprint(main_space)
@@ -125,7 +140,206 @@ def rBall_nBox(r,n):
 #         temp.append(comb(i,j))
 #     print(str(temp))
 
-rBall_nBox(7,3)
+# rBall_nBox(3,5)
 
-f = lambda x: comb(6,x)*(1/6**x)
-print(f(2))
+# f = lambda x: comb(6,x)*(1/6**x)
+# print(f(2))
+
+
+
+def exercise_1_9_1(word='MISSISSIPPI'):
+    base = [x for x in word]
+    base.sort()    
+    print(f'base is :{base}')
+    permut = permutations(base,len(base))
+    set_result = set(permut)
+    lst_result = [''.join(x) for x in set_result]
+    lst_result.sort()
+    
+    with open('test_mississippi.log','a') as f:
+        count = 1
+        for i in lst_result:
+            f.write(f'{count: 6} >> {i}\n')
+            count += 1
+    print(len(lst_result))
+    
+def exercise_1_9_3():
+    # perm = combinations(list(range(1,11)),5)
+    # fprint(perm)
+    perm2 = list(combinations_with_replacement(list(range(1,11)), 5))
+    prod = list(product(range(1,11),repeat=5))
+    def accepted(lst):
+        result = True
+        for i in range(0,len(lst)-1):
+            if lst[i]==lst[i+1]: return False
+        return result
+    # perm3 = [x for x in perm2 if accepted(x)]
+    prod_row = [x for x in prod if accepted(x)]
+    write_to_file(perm2)
+    # write_to_file(perm3,'test_row.log')
+    write_to_file(prod_row,'test_row.log')
+
+
+# exercise_1_9_3()
+
+def accepted(lst):
+    result = True
+    for i in range(0,len(lst)-1):
+        if lst[i]==lst[i+1]: return False
+    return result
+    
+def check_3_time(lst):
+    result = False
+    for i in lst:
+        if lst.count(i) == 3:
+            return True
+    return result
+
+# =============================================================================
+# this is for exercise : 3 from first chapter
+# =============================================================================
+# x = product([1,2,3,4,5,6,7,8,9,10],repeat=5)
+# # set_x = set(x)
+# # lst_x = list(x)
+# count = 1
+# count_sub = 1
+# with open('test.log','w') as f:
+#     for i in x:
+#         t =''.join([str(f) for f in i])
+#         s = set(i)
+#         if accepted(i): 
+#             # if: len(s) == 3 and not check_3_time(i):
+#                 # if 1 in i and 2 in i and 3 in i :
+#                     # if i.count(1) == 2 and i.count(2) == 2:
+#                         f.write(f'{count:4} >> {t}  accepted {count_sub}\n')
+#                         count_sub +=1
+#         # else:
+#         #     f.write(f'{count:4} >> {t}\n')
+#         count += 1
+# =============================================================================
+# exercise 3 finished here
+# =============================================================================
+    
+# x = product([1,2,3,4],repeat=4)    
+# print(50*'-')
+# lst_cnt = 1
+# count = 1
+# count_1 = 1
+# count_2_a = 1
+# count_2_b = 1
+# count_3 = 1
+# count_accept = 1
+# for i in x:
+#     s = set(i)
+#     l = list(i)
+#     if len(s) == 1:
+#         print(f'{lst_cnt:3}: {count:4} >> {i} ########### ONE {count_1}')
+#         count_1 +=1
+#         lst_cnt += 1
+#     elif len(s) == 2:
+#         a = s.pop()
+#         if l.count(a) == 1 or l.count(a) == 3:
+#             print(f'{lst_cnt:3}: {count:4} >> {i} ########### TWO  AAA_B {count_2_a}')
+#             count_2_a +=1
+#             lst_cnt += 1
+#         elif l.count(a) == 2 and not accepted(l):
+#             print(f'{lst_cnt:3}: {count:4} >> {i} ########### TWO  AA_BB {count_2_b}')
+#             count_2_b += 1
+#             lst_cnt += 1
+#         elif l.count(a) == 2 and accepted(l):
+#             print(f'{lst_cnt:3}: {count:4} >> {i} ACCEPTED {count_accept}')
+#             count_accept +=1
+#             lst_cnt +=1
+            
+#     elif len(s) == 3:
+#         if accepted(l):
+#             print(f'{lst_cnt:3}: {count:4} >> {i} ACCEPTED {count_accept}')
+#             count_accept +=1
+#             lst_cnt +=1
+#         else:
+#             print(f'{lst_cnt:3}: {count:4} >> {i} ########### THREE  AA_BB {count_3}')
+#             count_3 += 1
+#             lst_cnt +=1
+#     elif len(s) == 4:
+#         if accepted(l):
+#             print(f'{lst_cnt:3}: {count:4} >> {i} ACCEPTED {count_accept}')
+#             count_accept +=1
+#             lst_cnt +=1
+                 
+#         #     print(f'{count:4} >> {i}  accepted {count_sub}')
+#         #     count_sub +=1
+#         # else:
+#         #     print(f'{count:4} >> {i}')
+#     count += 1
+    
+
+# print(count_1-1)
+# print(count_2_a-1)
+# print(count_2_b-1)
+# print(count_3-1)
+# print(count_accept-1)
+# print(count_1+count_2_a+count_2_b+count_3)
+
+
+def check_for_exercise8(lst):
+    original = list(range(1,13))
+    temp = []
+    for i in lst:
+        for j in i:
+            temp.append(j)
+    temp.sort()
+    if temp == original:
+        return True
+    return False
+
+# x = list(range(1,13))
+# fprint(x)
+# base = [list(a) for a in combinations(x, 4)]
+# fprint(base)
+# test = [a for a in combinations(base, 3)]
+# count =1
+# for i in test:
+#     if check_for_exercise8(list(i)):
+#         print(f'{count} >>> {i}')
+#         count += 1
+    
+# print('Well done')
+
+# x = [list(a) for a in combinations([1,2,3,4,5], 2)]
+# fprint(x)
+
+def exercise_1_17():
+    f = ['a1','a2','a3','b1','b2','b3']
+    x =[a for a in permutations(f,len(f))]
+    # fprint(x)
+    def flipBin(test ='111000'):
+        result = test.replace('1','T')
+        result = result.replace('0','1')
+        return result.replace('T','0')
+    
+    def get_flag(a,model='111000',with_flip = True):
+        temp = ''
+        for i in a:
+            if i.startswith('a'): temp +='1'
+            else : temp += '0'
+        if with_flip:
+            if temp == model or temp == flipBin(model): return True
+        else:
+            if temp == model: return True
+        return False
+    count = 1
+    for i in x:
+        if get_flag(i,'101010'):
+            print(f'{count} >> {i}')
+            count +=1
+        
+    
+    
+    
+exercise_1_17()
+
+# for i in range(2,10):
+#     print(f'{i} comb({2*i},{i}) = {comb(2*i,i)}')
+
+
+
